67b356dc04c69d577f7c97c2826508163779a52f
nWhitcome <whitcome@iastate.edu>
2020-04-25 01:27:38 -0500

    added metronome functionality aside from volume changes on accents

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,39 @@
 <template>
   <div id="app">
-    <!--<h1>Metronome: <input class="textInput" type="number" min="10" max="300" v-model="metronome">BPM</h1>-->
+    <h1>
+      Metronome:
+      <input class="textInput" type="number" min="10" max="300" v-model="metronome.bpm" />BPM
+    </h1>
+    <div class="arrowHolder noselect">
+        <div class="arrowInnerBox">
+          <i @click="playPause()" style="cursor: pointer">{{ getPlayPauseIcon }}</i>
+        </div>
+      </div>
     <b style="font-size: 26px; margin: 10px; color: #333">{{ numHitsVar }}</b>
     <div class="slideContainer">
-      <input type="range" min="3" v-bind:max="maxHits" v-model="numHitsVar" class="slider" id="numHits">
+      <input
+        type="range"
+        min="3"
+        v-bind:max="maxHits"
+        v-model="numHitsVar"
+        class="slider"
+        id="numHits"
+      />
     </div>
     <div class="generalBox">
       <div class="hitsBox">
-        <DrumHit class="noselect" 
-        v-for="(n, i) in patternArray" 
-        :hitType='patternArray[i]' 
-        v-show="i < numHitsVar" 
-        :key="'item' + i + keyUpdate"
-        :hover='highlighted'
-        :index='i'
-        @mouseover.native="highlighted = i"
-        @mouseleave.native="highlighted = -1"
-        @click.native="changeLetter(i)"></DrumHit>
+        <DrumHit
+          class="noselect"
+          v-for="(n, i) in patternArray"
+          :hitType="patternArray[i]"
+          v-show="i < numHitsVar"
+          :key="'item' + i + keyUpdate"
+          :hover="highlighted"
+          :index="i"
+          @mouseover.native="highlighted.one = i"
+          @mouseleave.native="highlighted.one = -1"
+          @click.native="changeLetter(i)"
+        ></DrumHit>
       </div>
       <div @click="genPattern()" id="genButton" class="noselect">Generate</div>
     </div>
@@ -24,61 +41,165 @@
 </template>
 
 <script>
-import DrumHit from './components/DrumHit.vue';
+import DrumHit from "./components/DrumHit.vue";
 export default {
-  name: 'app',
+  name: "app",
   components: { DrumHit },
-  data () {
+  data() {
     return {
       numHitsVar: 5,
-      possArray: ['L', 'R', 'K'],
+      possArray: ["L", "R", "K"],
       patternArray: [],
-      highlighted: -1, 
+      highlighted: {
+        one: -1,
+        two: -1
+      },
       keyUpdate: 0,
-      metronome: 120,
+      metronome: {
+        bpm: 120,
+        interval: null,
+        index: 0,
+      },
+      isPlaying: false,
       maxHits: 15,
-    }
+      audioFiles: {
+        L: new Howl({ src: "src/audio/mid.mp3" }),
+        R: new Howl({ src: "src/audio/high.mp3" }),
+        K: new Howl({ src: "src/audio/low.mp3" }),
+      }
+    };
   },
   methods: {
     /*
      * Generates the sticking pattern and places it into an array. Makes sure there are no triplets of the same hit.
      */
-    genPattern: function () {
+    genPattern: function() {
       this.patternArray = [];
-      while(this.patternArray.length < this.maxHits){
-        var letter = this.possArray[Math.floor(Math.random() * this.possArray.length)];
+      while (this.patternArray.length < this.maxHits) {
+        var letter = this.possArray[
+          Math.floor(Math.random() * this.possArray.length)
+        ];
         var patLength = this.patternArray.length;
 
         //Checks if the previous two items in the array match the one that is about to be added and changes it if it is
-        if(patLength > 1){
-          if(this.patternArray[patLength - 2].localeCompare(letter) == 0 && this.patternArray[patLength - 1].localeCompare(letter) == 0){
+        if (patLength > 1) {
+          if (
+            this.patternArray[patLength - 2].localeCompare(letter) == 0 &&
+            this.patternArray[patLength - 1].localeCompare(letter) == 0
+          ) {
             var dupArray = this.possArray.slice();
             dupArray.splice(dupArray.indexOf(letter), 1);
             letter = dupArray[Math.floor(Math.random() * dupArray.length)];
           }
         }
-        this.patternArray.push(letter)
+        this.patternArray.push(letter);
       }
     },
-    changeLetter: function(i){
+    changeLetter: function(i) {
       //var audio = new Audio("./src/audio/mid.mp3");
       //audio.play();
-      this.patternArray[i] = this.possArray[(this.possArray.indexOf(this.patternArray[i]) + 1) % this.possArray.length];
+      this.patternArray[i] = this.possArray[
+        (this.possArray.indexOf(this.patternArray[i]) + 1) %
+          this.possArray.length
+      ];
       this.keyUpdate++;
     },
+    playPause: function(){
+      if(this.isPlaying){
+        this.isPlaying = false;
+      }
+      else{
+        this.isPlaying = true;
+      }
+    },
+    playSound(){
+      var nextSound = this.highlighted.two + 1;
+      if(this.highlighted.two < this.numHitsVar - 1){
+        this.highlighted.two++;
+      }
+      else{
+        nextSound = 0;
+        this.highlighted.two = 0;
+      }
+      if(this.patternArray[nextSound].localeCompare("L") == 0){
+        this.audioFiles.L.play();
+      }
+      else if(this.patternArray[nextSound].localeCompare("R") == 0){
+        this.audioFiles.R.play();
+      }
+      else if(this.patternArray[nextSound].localeCompare("K") == 0){
+        this.audioFiles.K.play();
+      }
+    }
+  },
+  computed: {
+    getPlayPauseIcon: function(){
+      if(this.isPlaying){
+        clearInterval(this.metronome.interval);
+        this.metronome.interval = setInterval(() => {this.playSound()}, (60 * 1000)/(this.metronome.bpm));
+        return "pause";
+      }
+      else{
+        this.highlighted.two = -1;
+        clearInterval(this.metronome.interval);
+        return "play_arrow";
+      }
+    }
   },
   beforeMount() {
     this.genPattern();
-  },
-}
+  }
+};
 </script>
 
 <style lang="scss">
 $blue-general: #2471a3;
 $blue-dark: #1c5a82;
 
+@font-face {
+  font-family: "Material_Icons";
+  src: url("/src/fonts/Material_Icons.woff2") format("woff2");
+  font-weight: normal;
+  font-style: normal;
+}
+
+.arrowHolder{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100px;
+  font-size: 50px;
+}
+
+.arrowInnerBox{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 60px;
+  height: 60px;
+  border-radius: 50%;
+  transition: .2s;
+  color: $blue_general;
+}
+
+.arrowInnerBox:hover{
+  border: 4px solid $blue_general;
+}
+
+.arrowInnerBox:active{
+  transition: 0s;
+  border: 4px solid $blue_general;
+  background-color: $blue_general;
+  color: white;
+}
+
+i {
+  font-family: "Material_Icons";
+  font-style: normal;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -102,8 +223,8 @@ $button-height: 2em;
   line-height: $button-height;
   margin: 20px auto;
   opacity: 0.8;
-  -webkit-transition: .2s;
-  transition: opacity .2s;
+  -webkit-transition: 0.2s;
+  transition: opacity 0.2s;
 }
 
 #genButton:active {
@@ -112,7 +233,8 @@ $button-height: 2em;
   background-color: $blue-dark;
 }
 
-.hitsBox, .generalBox {
+.hitsBox,
+.generalBox {
   display: flex;
   flex-wrap: wrap;
   align-items: center;
@@ -129,11 +251,11 @@ $button-height: 2em;
 
 .noselect {
   -webkit-touch-callout: none; /* iOS Safari */
-    -webkit-user-select: none; /* Safari */
-     -khtml-user-select: none; /* Konqueror HTML */
-       -moz-user-select: none; /* Old versions of Firefox */
-        -ms-user-select: none; /* Internet Explorer/Edge */
-            user-select: none; /* Non-prefixed version, currently
+  -webkit-user-select: none; /* Safari */
+  -khtml-user-select: none; /* Konqueror HTML */
+  -moz-user-select: none; /* Old versions of Firefox */
+  -ms-user-select: none; /* Internet Explorer/Edge */
+  user-select: none; /* Non-prefixed version, currently
                                   supported by Chrome, Opera and Firefox */
 }
 
@@ -144,37 +266,41 @@ $button-height: 2em;
 
 /* The slider itself */
 .slider {
-  -webkit-appearance: none;  /* Override default CSS styles */
+  z-index: 2;
+  -webkit-appearance: none; /* Override default CSS styles */
   appearance: none;
   width: 100%; /* Full-width */
   max-width: 500px;
-  height: 2px; /* Specified height */
+  height: 4px; /* Specified height */
   background: #d3d3d3; /* Grey background */
   outline: none; /* Remove outline */
   opacity: 0.8; /* Set transparency (for mouse-over effects on hover) */
-  -webkit-transition: .2s; /* 0.2 seconds transition on hover */
-  transition: opacity .2s;
+  -webkit-transition: 0.2s; /* 0.2 seconds transition on hover */
+  transition: opacity 0.2s;
   margin: 10px 0 20px 0;
 }
 
 /* Mouse-over effects */
-.slider:hover, #genButton:hover {
+.slider:hover,
+#genButton:hover {
   opacity: 1; /* Fully shown on mouse-over */
 }
 
 .slider::-webkit-slider-thumb {
+  z-index: 2;
   -webkit-appearance: none; /* Override default look */
   appearance: none;
-  width: 15px; /* Set a specific slider handle width */
-  height: 15px; /* Slider handle height */
+  width: 20px; /* Set a specific slider handle width */
+  height: 20px; /* Slider handle height */
   background: $blue-general; /* Green background */
   cursor: pointer; /* Cursor on hover */
   border-radius: 50%;
 }
 
 .slider::-moz-range-thumb {
-  width: 15px; /* Set a specific slider handle width */
-  height: 15px; /* Slider handle height */
+  z-index: 2;
+  width: 20px; /* Set a specific slider handle width */
+  height: 20px; /* Slider handle height */
   background: $blue-general; /* Green background */
   cursor: pointer; /* Cursor on hover */
   border-radius: 50%;
--- src/components/DrumHit.vue
+++ src/components/DrumHit.vue
@@ -1,75 +1,77 @@
 <template>
-    <div v-bind:class="(hover == index || hover == -1) ? 'backBox' : 'backBoxSmall'">
-        <p v-bind:class="(hover == index || hover == -1) ? 'centerLetter' : 'centerLetterSmall'">{{ hitType }}</p>
-    </div>
+  <div v-bind:class="(hover.one == index || hover.two == index || (hover.one == -1 && hover.two == -1)) ? 'backBox' : 'backBoxSmall'">
+    <p
+      v-bind:class="(hover.one == index || hover.two == index || (hover.one == -1 && hover.two == -1)) ? 'centerLetter' : 'centerLetterSmall'"
+    >{{ hitType }}</p>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'DrumHit',
-    props: ['hitType', 'active', 'hover', 'index'],
-    data() {
-        return{
-            hitLetter: this.hitType,
-        }
-    },
-    watch:{
-        data(val){
-            this.hitLetter=val
-        }
-    },
-}
+  name: "DrumHit",
+  props: ["hitType", "active", "hover", "index"],
+  data() {
+    return {
+      hitLetter: this.hitType
+    };
+  },
+  watch: {
+    data(val) {
+      this.hitLetter = val;
+    }
+  }
+};
 </script>
 
 <style lang="scss">
 .backBox {
-    background-color: #DDD;
-    color: #333;
-    width: 4em;
-    height: 6em;
-    border-radius: 1em;
-    margin: 1em;
-    z-index: 1;
-    transition: .2s;
-    cursor: pointer;
+  background-color: #ddd;
+  color: #333;
+  width: 4em;
+  height: 6em;
+  border-radius: 1em;
+  margin: 1em;
+  z-index: 1;
+  transition: 0.2s;
+  cursor: pointer;
 }
 
-.backBox:active{
-    background-color: #CCC;
-    transition: 0s;
+.backBox:active {
+  background-color: #ccc;
+  transition: 0s;
 }
 
 .backBoxSmall {
-    background-color: #EAEAEA;
-    color: #888;
-    width: 3.5em;
-    height: 5.5em;
-    border-radius: 1em;
-    margin: 1.25em;
-    z-index: 1;
-    transition: .2s;
-    cursor: pointer;
+  background-color: #eaeaea;
+  color: #888;
+  width: 3.5em;
+  height: 5.5em;
+  border-radius: 1em;
+  margin: 1.25em;
+  z-index: 1;
+  transition: 0.2s;
+  cursor: pointer;
 }
 
 .centerLetter {
-    font-size: 1.9em;
-    width: 100%;
-    height: 100%;
-    margin: 0px;
-    transition: .2s;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  font-size: 1.9em;
+  width: 100%;
+  height: 100%;
+  margin: 0px;
+  transition: 0.2s;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
 .centerLetterSmall {
-    font-size: 1.8em;
-    width: 100%;
-    height: 100%;
-    margin: 0px;
-    transition: .2s;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  font-size: 1.8em;
+  width: 100%;
+  height: 100%;
+  margin: 0px;
+  transition: 0.2s;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 </style>
